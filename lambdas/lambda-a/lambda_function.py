a7baa3c0f2d7ed8e1243518d4d02523d77e1c408
Jose Bryan <jbsg97@gmail.com>
2025-06-10 12:35:41 -0700

    feat: Changed lambda a response

--- lambdas/lambda-a/lambda_function.py
+++ lambdas/lambda-a/lambda_function.py
@@ -5,20 +5,21 @@ import json
 def lambda_handler(event, context):
     lambda_client = boto3.client('lambda')
     
-    # Get current feature/stage from environment
+    # Get current feature/stage and environment
     feature = os.environ.get('FEATURE_NAME', '')
+    env = os.environ.get('ENVIRONMENT', '')
     
     # If in feature branch, use feature alias
     qualifier = f"feature-{feature}" if feature else None
     
     response = lambda_client.invoke(
-        FunctionName='lambda-b',
+        FunctionName=f'lambda-b_{env}',
         Qualifier=qualifier,  # Will use $LATEST if None
         InvocationType='RequestResponse',
         Payload=json.dumps(event)
     )
     return {
         'statusCode': 200,
-        'body': 'Testing lambda-b response',
+        'body': f'Testing lambda-b_{env} response',
         'lambda_response': json.loads(response['Payload'].read())
     }
